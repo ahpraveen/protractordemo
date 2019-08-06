4bec00a6aff6d1793ef6b4182c58e2b75445469f
Praveen Anna Haridas <aha.praveen@gmail.com>
2019-08-06 09:58:17 -0400

    updated the relative path

--- configuration/conf_firefox_1.js
+++ configuration/conf_firefox_1.js
@@ -1,7 +1,7 @@
 exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['to-do-list_spec.js'],
+  specs: ['../tests/to-do-list_spec.js'],
   capabilities: {
     browserName: 'firefox'
   }
