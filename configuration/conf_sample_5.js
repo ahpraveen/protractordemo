e3d27a4f06a943655f27655d31a65560bc5cc0b3
Praveen Anna Haridas <aha.praveen@gmail.com>
2019-08-06 10:01:27 -0400

    Sample 5

--- configuration/conf_sample_5.js
+++ configuration/conf_sample_5.js
@@ -1,7 +1,7 @@
 exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['testPageObjectspec.js'],
+  specs: ['../tests/testPageObjectspec.js'],
   capabilities: {
   browserName: 'firefox'
   },
